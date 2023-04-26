7b67f9bc7249175f1ee57e08f2c2c1ddc98c882f
Alberto <albertogbeltre@gmail.com>
2023-04-26 16:30:51 -0400

    implementing tailwind styles and public files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,39 @@ export default {
   content: [ "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        /*Primary*/
+
+        "BrightBlue": "hsl(220, 98%, 61%)",
+        "CheckBackground": "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+
+        /*Light Theme*/
+
+        "VeryLightGray": "hsl(0, 0%, 98%)",
+        "VeryLightGrayishBlue": "hsl(236, 33%, 92%)",
+        "LightGrayishBlue": "hsl(233, 11%, 84%)",
+        "DarkGrayishBlue": "hsl(236, 9%, 61%)",
+        "VeryDarkGrayishBlue": "hsl(235, 19%, 35%)",
+
+        /*Dark Theme*/
+
+        "VeryDarkBlue":" hsl(235, 21%, 11%)",
+        "VeryDarkDesaturatedBlue": "hsl(235, 24%, 19%)",
+        "LightGrayishBlue": "hsl(234, 39%, 85%)",
+        "LightGrayishBlue(hover)": "hsl(236, 33%, 92%)",
+        "DarkGrayishBlue": "hsl(234, 11%, 52%)",
+        "VeryDarkGrayishBlue": "hsl(233, 14%, 35%)",
+        "VeryDarkGrayishBlue": "hsl(237, 14%, 26%)"
+      },
+
+      backgroundImage:{
+        "mobileDark" : "url('./public/bg-mobile-dark.jpg')",
+        "mobileLight" : "url('./public/bg-mobile-light.jpg')",
+        "desktopDark" : "url('./public/bg-desktop-dark.jpg')",
+        "desktopLight" : "url('./public/bg-desktop-light.jpg')"
+      }
+    },
   },
   plugins: [],
 }
